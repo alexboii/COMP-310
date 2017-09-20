b631d355658ef2bffd10fb05820780af60146159
alexboii <potteref@gmail.com>
2017-09-19 23:18:53 -0400

    added error handling for input

--- assignment_1/simpleshell.c
+++ assignment_1/simpleshell.c
@@ -2,19 +2,34 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-//// This code is given for illustration purposes. You need not include or follow this
-// strictly. Feel free to writer better or bug free code. This example code block does not
-// worry about deallocating memory. You need to ensure memory is allocated and deallocated
-// properly so that your shell works without leaking memory.
-//
+
+// CONSTANTS 
+#define PRESENTATION "Alexander Bratyshkin --- 260684228 --- Assignment 1 --- ECSE 427 "
+#define ERROR_MESSAGE_DEFAULT "An error has occurred, exiting shell..."
+#define ERROR_MESSAGE_NO_ARGS "You haven't entered a command. Please try again."
+
+// FUNCTION PROTOTYPES 
+void prompt_message(char* message);
+
+
+/** 
+ * @brief Function provided by Prof. Harmouche
+ * @note   
+ * @param  *prompt: 
+ * @param  *args[]: 
+ * @param  *background: 
+ * @retval Number of arguments
+  */
 int getcmd(char *prompt, char *args[], int *background)
 {
     int length, i = 0;
     char *token, *loc;
     char *line = NULL;
     size_t linecap = 0;
+
     printf("%s", prompt);
     length = getline(&line, &linecap, stdin);
+
     if (length <= 0)
     {
         exit(-1);
@@ -38,10 +53,10 @@ int getcmd(char *prompt, char *args[], int *background)
             {
                 token[j] = '\0';
             }
-            if (strlen(token) > 0)
-            {
-                args[i++] = token;
-            }
+        }
+        if (strlen(token) > 0)
+        {
+            args[i++] = token;
         }
     }
 
@@ -52,11 +67,32 @@ int main(void)
 {
     char *args[20];
     int bg;
+    prompt_message(PRESENTATION);
     while (1)
     {
         bg = 0;
         int cnt = getcmd("\n>> ", args, &bg);
-        printf("%c", cnt);
-        /* the steps can be..:(1) fork a child process using fork()(2) the child process will invoke execvp()(3) if background is not specified, the parent will wait,otherwise parent starts the next command... */
+
+        for (int i = 0; i < cnt; i++)
+            printf("\nArg[%d] = %s", i, args[i]);
+
+        // error handling for command input    
+        if(cnt == -1)
+        {
+            prompt_message(ERROR_MESSAGE_DEFAULT);
+        }
+        
+        if(cnt == 0)
+        {
+            prompt_message(ERROR_MESSAGE_NO_ARGS);
+            continue;
+        }
     }
 }
+
+// HELPER SECTION
+
+void prompt_message(char* message){
+    printf("%s", message);
+}
+
