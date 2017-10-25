408e66f21a4052d3a8f09d4baa42f921f3ab7c19
alexboii <potteref@gmail.com>
2017-10-24 23:20:03 -0400

    added command parsing
    TODO: Implement all functionalities but for one thread only

---
+++ assignment_2/assignment_2.c
@@ -0,0 +1,80 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+
+// FUNCTION PROTOTYPES
+int getcmd(char *prompt, char *args[], int *background);
+
+int main(void)
+{
+    char *args[20];
+    int bg;
+
+    while (1)
+    {
+        int cnt = getcmd("\n>> ", args, &bg);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/** 
+ * @brief Function provided by Prof. Harmouche
+ * @note   
+ * @param  *prompt: 
+ * @param  *args[]: 
+ * @param  *background: 
+ * @retval Number of arguments
+  */
+int getcmd(char *prompt, char *args[], int *background)
+{
+    int length, i = 0;
+    char *token, *loc;
+    char *line = NULL;
+    size_t linecap = 0;
+
+    printf("%s", prompt);
+    length = getline(&line, &linecap, stdin);
+
+    if (length <= 0)
+    {
+        exit(-1);
+    }
+    // Check if background is specified..
+    if ((loc = index(line, '&')) != NULL)
+    {
+        *background = 1;
+        *loc = ' ';
+    }
+    else
+    {
+        *background = 0;
+    }
+
+    while ((token = strsep(&line, " \t\n")) != NULL)
+    {
+        for (int j = 0; j < strlen(token); j++)
+        {
+            if (token[j] <= 32)
+            {
+                token[j] = '\0';
+            }
+        }
+        if (strlen(token) > 0)
+        {
+            args[i++] = token;
+        }
+    }
+
+    free(token);
+    free(line);
+
+    return i;
+}
