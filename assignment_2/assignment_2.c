f229a20b89322417e7018a2d15fd9a05357b773d
alexboii <potteref@gmail.com>
2017-10-28 22:39:01 -0400

    added writer/reader problem solution

--- assignment_2/assignment_2.c
+++ assignment_2/assignment_2.c
@@ -18,6 +18,8 @@
 typedef struct
 {
     char reservations[20][30];
+    int sem_count;
+    int reader_sem_count;
 } Tables;
 
 // #1 HASH CONSTANTS FOR COMMANDS
@@ -33,8 +35,8 @@ int MEMORY_SIZE = 20 * sizeof(Tables);
 
 // #3 NAME CONSTANTS
 #define MEMORY_NAME "/abraty"
-#define SEM_NAME "/abraty_sem"
-#define SEM_COUNT_NAME "/abraty_sem_count"
+#define SEM_RESOURCE_NAME "/abraty_sem_resource"
+#define SEM_READ_COUNT_NAME "/abraty_sem_read"
 
 // #4 ERROR MESSAGE CONSTANTS
 #define ERROR_SMH_OPEN "Could not perform smh_open.\n"
@@ -42,10 +44,22 @@ int MEMORY_SIZE = 20 * sizeof(Tables);
 #define ERROR_FTRUNCANTE "Could not perform ftruncate.\n"
 #define ERROR_CLOSE "Could not close file descriptor.\n"
 #define ERROR_OPEN_SEMAPHORE "Could not open semaphore.\n"
+#define ERROR_WRONG_COMMAND "You have entered an invalid command. Supported commands: \n"
+#define ERROR_SEM_SIGNAL "Could not send signal from semaphore.\n"
+#define ERROR_SEM_WAIT "Could not enter critical section.\n"
+#define ERROR_SHARE_MEMORY "Could not initialize shared memory. Exiting...\n"
+#define ERROR_READING "Could not perform read operation.\n"
 
 // #5 DEBUG STATEMENTS
-#define CREATION_OR_READ_SUCCESS "Sucessfully created or opened shared memory.\n"
-#define OPENING_EXISTING_MEMORY "Memory already created, allocating in O_RWRD mode...\n"
+#define DEBUG_PREFIX "DEBUG: "
+#define CREATION_OR_READ_SUCCESS DEBUG_PREFIX "Sucessfully created or opened shared memory.\n"
+#define OPENING_EXISTING_MEMORY DEBUG_PREFIX "Memory already created, allocating in O_RWRD mode...\n"
+#define CS_ATTEMPT DEBUG_PREFIX "Attempting to enter critical section...\n"
+#define CS_ENTERED DEBUG_PREFIX "Entered critical section.\n"
+#define CS_SIGNAL DEBUG_PREFIX "Exiting critical section & signaling departure from critical section...\n"
+#define CALLBACK DEBUG_PREFIX "Executing callback...\n"
+#define WRITER_FLAG DEBUG_PREFIX "Writer.\n"
+#define READER_FLAG DEBUG_PREFIX "Reader.\n"
 
 // #6 FUNCTION PROTOTYPES
 int getcmd(char *prompt, char *args[]);
@@ -54,15 +68,21 @@ unsigned long hash(unsigned char *str);
 int initialize_tables();
 int make_reservation(char *table, char *name);
 int read_all();
+int initialize_semaphore(char *sem_name, int value, sem_t **semaphore, void (*callback)(sem_t **));
+int sem_wait_wrapper(sem_t **semaphore);
+int sem_post_wrapper(sem_t **semaphore);
+int reader(int (*read_operation)());
+int writer(int (*write_operation)());
 
 // #7 GLOBAL VARIABLES
 Tables *global_tables;
-int DEBUG = 1; // toggle debug statements
-sem_t *sem_count;
-sem_t *sem_proc;
+int DEBUG = 1;              // toggle debug statements
+sem_t *sem_resource_access; // access semaphore
+sem_t *sem_read_count;      // semaphore for the read counter
 
 // #9 MISC
 #define LEN(arr) ((int)(sizeof(arr) / sizeof(arr)[0]))
+#define LAMBDA(c_) ({ c_ _; })
 
 #pragma endregion
 
@@ -73,9 +93,19 @@ int main(void)
 {
     char *args[20];
 
-    if (initialize_tables() != 0 && DEBUG)
+    // TODO: Setup signal handler to unlink shared memory and decrement semaphores whatnot
+
+    if (!initialize_tables())
     {
-        printf(CREATION_OR_READ_SUCCESS);
+        perror(ERROR_SHARE_MEMORY);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        if (DEBUG)
+        {
+            printf(CREATION_OR_READ_SUCCESS);
+        }
     }
 
     while (1)
@@ -104,18 +134,31 @@ int execute_command(char *args[], int args_length)
         printf("%s = %lu\n", args[0], argument);
     }
 
+    if (argument == EXIT_HASH)
+    {
+        return 0;
+    }
+
+    if (argument != RESERVE_HASH && argument != STATUS_HASH && argument != INIT_HASH)
+    {
+        perror(ERROR_WRONG_COMMAND);
+        return -1;
+    }
+
     switch (argument)
     {
     case RESERVE_HASH:
-        make_reservation(args[1], args[2]);
+        // TODO: Create validate reservation command
+        writer(LAMBDA(int _() { return make_reservation(args[1], args[2]); }));
         break;
     case STATUS_HASH:
-        read_all();
+        reader(LAMBDA(int _() { return read_all(); }));
         break;
     default:
-        return 0;
-        break;
+        return -1;
     }
+
+    return 1;
 }
 #pragma endregion
 
@@ -135,27 +178,20 @@ int initialize_tables()
         return 0;
     }
 
-    sem_count = sem_open(SEM_COUNT_NAME, O_RDWR | O_CREAT, S_IRWXU, 1);
-
-    if (sem_count == SEM_FAILED)
-    {
-        perror(ERROR_OPEN_SEMAPHORE);
-        return 0;
-    }
-
-    // signal that a new process has been created
-    sem_post(sem_count);
+    // this function doesn't work on some machines, so have to change it
+    // // open semaphore that contains the number of processes using the shared memory
+    // initialize_semaphore(SEM_COUNT_NAME, 0, &sem_count, LAMBDA(void _(sem_t * *sem) {
+    //                          // signal that a new process has been created
+    //                          sem_post(*sem);
+    //                      }));
 
     // attach current semaphore
-    sem_proc = sem_open(SEM_COUNT_NAME, O_RDWR | O_CREAT, S_IRWXU, 1);
+    initialize_semaphore(SEM_RESOURCE_NAME, 1, &sem_resource_access, NULL);
 
-    if (sem_proc == SEM_FAILED)
-    {
-        perror(ERROR_OPEN_SEMAPHORE);
-        return 0;
-    }
+    // attach reader count semaphore
+    initialize_semaphore(SEM_READ_COUNT_NAME, 1, &sem_read_count, NULL);
 
-    sem_wait(sem_proc);
+    // TODO: Add resource control in here, maybe call reader?
 
     global_tables = mmap(0, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
@@ -177,7 +213,7 @@ int initialize_tables()
         return 0;
     }
 
-    sem_post(sem_proc);
+    global_tables->sem_count = global_tables->sem_count + 2; // since we have a semaphore the readcounter and an access resource semaphore
 
     return 1;
 }
@@ -203,6 +239,156 @@ int read_all()
             printf("TABLE %d RESERVED FOR USER %s\n", i, global_tables->reservations[i]);
         }
     }
+
+    return 1;
+}
+
+int initialize_semaphore(char *sem_name, int value, sem_t **semaphore, void (*callback)(sem_t **))
+{
+    *semaphore = sem_open(sem_name, O_RDWR | O_CREAT, S_IRWXU, value);
+
+    if (*semaphore == SEM_FAILED)
+    {
+        perror(ERROR_OPEN_SEMAPHORE);
+        return 0;
+    }
+
+    if (callback != NULL)
+    {
+        if (DEBUG)
+        {
+            printf(CALLBACK);
+        }
+
+        callback(semaphore);
+    }
+}
+
+int sem_wait_wrapper(sem_t **semaphore)
+{
+    if (DEBUG)
+    {
+        printf(CS_ATTEMPT);
+    }
+
+    if (sem_wait(*semaphore) == -1)
+    {
+        perror(ERROR_SEM_WAIT);
+        return 0;
+    }
+
+    if (DEBUG)
+    {
+        printf(CS_ENTERED);
+    }
+
+    return 1;
+}
+
+int sem_post_wrapper(sem_t **semaphore)
+{
+    if (sem_post(*semaphore) == -1)
+    {
+        perror(ERROR_SEM_SIGNAL);
+        return 0;
+    }
+
+    if (DEBUG)
+    {
+        printf(CS_SIGNAL);
+    }
+
+    return 1;
+}
+
+#pragma endregion
+
+#pragma region READER AND WRITER FUNCTIONS
+
+int reader(int (*read_operation)())
+{
+
+    if (DEBUG)
+    {
+        printf(READER_FLAG);
+    }
+
+    if (sem_wait_wrapper(&sem_read_count) == 0)
+    {
+        return 0;
+    }
+
+    global_tables->reader_sem_count++;
+
+    if (global_tables->reader_sem_count == 1)
+    {
+        if (sem_wait_wrapper(&sem_resource_access) == 0)
+        {
+            return 0;
+        }
+    }
+
+    if (sem_post_wrapper(&sem_read_count) == 0)
+    {
+        return 0;
+    }
+
+    read_operation();
+
+    if (DEBUG)
+    {
+        sleep(5);
+    }
+
+    if (sem_wait_wrapper(&sem_read_count) == 0)
+    {
+        return 0;
+    }
+
+    global_tables->reader_sem_count--;
+
+    if (global_tables->reader_sem_count == 0)
+    {
+        if (sem_post_wrapper(&sem_resource_access) == 0)
+        {
+            return 0;
+        }
+    }
+
+    if (sem_post_wrapper(&sem_read_count) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int writer(int (*write_operation)())
+{
+    if (DEBUG)
+    {
+        printf(WRITER_FLAG);
+    }
+
+    if (sem_wait_wrapper(&sem_resource_access) == 0)
+    {
+        return 0;
+    }
+
+    // critical write operation
+    write_operation();
+
+    if (DEBUG)
+    {
+        sleep(5);
+    }
+
+    if (sem_post_wrapper(&sem_resource_access) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
 }
 
 #pragma endregion
