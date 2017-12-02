efae4d493972768525ddda387e31d0eeb2c0c944
alexboii <potteref@gmail.com>
2017-12-01 21:11:18 -0500

    added open and close

--- assignment_3/sfs_api.c
+++ assignment_3/sfs_api.c
@@ -1,15 +1,22 @@
 
+// ALEXANDER BRATYSHKIN
+// 260684228
+
 #include "sfs_api.h"
 
-// #define DEBUG // toggle debug statements
+#define DEBUG // toggle debug statements
 
 //initialize all bits to high
 
 #pragma region GLOBAL DEFINITIONS
 
-superblock_t sb;
-inode_t inode_table[INODE_NO];
-entry_t dir_table[NUM_INODES - 1];
+// seriously, sorry again for these global variables, but I think it is forgiveable in this assignment since we're implementing an API, no main()
+superblock_t *sb;
+inode_t inode_table[INODES];
+directory_entry root[INODES];
+file_descriptor fd_table[INODES];
+uint8_t free_bit_map[BITMAP_ROW_SIZE] = {[0 ... BITMAP_ROW_SIZE - 1] = UINT8_MAX};
+int current_position = 0;
 
 #pragma endregion
 
@@ -19,96 +26,382 @@ void mksfs(int fresh)
 {
     if (fresh)
     {
-        D printf("Making a new file system \n");
+        D printf("New file system \n");
 
         // format virtual disk provided by emulator
-        init_fresh_disk(JITS_DISK, BLOCK_SIZE, NUM_BLOCKS);
+        assert(init_fresh_disk(LASTNAME_FIRSTNAME_DISK, BLOCK_SIZE, NUM_BLOCKS));
 
         // initialize superblock
-        superblock_init();
+        assert(superblock_init());
         force_set_index(0);
-        write_blocks(0, 1, (void *)&sb);
 
-        // initialize node table
-        for (int i = 1; i < ARRAYSIZE(inode_table); i++)
+        // initialize inode table
+        for (int i = 0; i < ARRAYSIZE(inode_table); i++)
         {
-            inode_table->free = 1;
+            inode_table->empty = 1;
         }
 
+        // initialize fd table
+        for (int i = 0; i < ARRAYSIZE(fd_table); i++)
+        {
+            fd_table->empty = 1;
+        }
+
+        set_fd(0, 0, 0, &inode_table[0], 0);
+
         // initialize node table
-        for (i = 1; i < INODE_BLOCKS_NO; i++)
+        for (int i = 1; i < INODE_BLOCKS_NO; i++) // start from 1 because first block for sb
         {
             force_set_index(i);
         }
 
-        // initialize root directory
-        inode_table[0] = (inode_t){.mode = 777, .link_cnt = 1, .uid = 0, .gid = 0, .size = 0, .free = 0};
-
-        // initialize directory table
-        for (i = 0; i < ARRAYSIZE(dir_table); i++)
-        {
-            inode_table->free = 1;
-        }
-
-        for (i = 0; i < DIR_BLOCKS_NO; i++)
+        // initialize directory's pointers for root
+        for (int i = 0; i < DIR_BLOCKS; i++)
         {
             int inode_wrapper = i + INODE_BLOCKS_NO + 1;
-            // initialize inode pointers from root
             inode_table[0].data_ptrs[i] = (unsigned int)inode_wrapper;
-            force_set_index(inode_wrapper);
+            force_set_index(inode_wrapper); // store root directory in bitmap
         }
 
-        for (i = 0; i < POINTER_SIZE; i++)
+        // store inode table in bitmap
+        for (int i = 1; i < FS_SIZE_INITIAL + 1; i++) // start from 1 because first block for sb
         {
-            if (inode_table[0].data_ptrs[i] == NULL)
-            {
-                inode_table[0].data_ptrs[i] = DUMMY_INITIALIZER;
-            }
+            force_set_index(i);
         }
 
-        // TODO: Create method => store_in_memory();
-        // write bitmap into memory block?
+        // store bitmap itself in bitamp
+        force_set_index(NUM_BLOCKS - 1);
+
+        assert(store_in_disk());
 
         return;
     }
+
+    D printf("Open existing file system \n");
+
+    assert(read_blocks(0, 1, &sb));
+
+    return;
 }
 
 int sfs_getnextfilename(char *fname)
 {
+    D printf("Inside of sfs_getnextfilename");
+
+    // TODO: Make this cleaner, better
+
+    if (root[current_position].empty == 1 || current_position >= INODES)
+    {
+        printf(CANNOT_FIND_FILE);
+        return 0;
+    }
+
+    current_position++;
+    strcpy(fname, root[current_position].name);
+
+    return 1;
 }
+
 int sfs_getfilesize(const char *path)
 {
+    int inode_num = find_file_inode_table((char *)path);
+
+    if (inode_num == -1)
+    {
+        printf(CANNOT_FIND_FILE);
+        return 0;
+    }
+
+    return inode_table[inode_num].size;
 }
+
 int sfs_fopen(char *name)
 {
+    D printf("I'm in sfs_fopen\n");
+
+    if (strlen(name) > MAX_FILE_NAME || strlen(name) == 0)
+    {
+        printf(INVALID_FILE_NAME);
+        return -1;
+    }
+
+    int inode_num = find_file_inode_table(name);
+
+    // if the file exists (begin by this condition because we're more likely to open a file that exists than try to open one that doesn't)
+    if (inode_num != -1)
+    {
+        int fd_num = find_file_fd_table(inode_num);
+        if (fd_num != -1)
+        {
+            // if the file is already open, we return it
+            return fd_num;
+        }
+
+        // find first non-used entry if the file isn't open
+        int empty_fd = find_empty_fd();
+
+        if (empty_fd == -1)
+        {
+            printf(TABLE_FULL);
+            return -1;
+        }
+
+        // we open the file
+        set_fd(empty_fd, 0, inode_num, &inode_table[inode_num], inode_table[inode_num].size); // set file to append mode
+
+        return empty_fd;
+    }
+
+    int empty_inode = find_empty_inode();
+
+    if (empty_inode == -1)
+    {
+        printf(INODE_TABLE_FULL);
+        return -1;
+    }
+
+    int empty_root_entry = find_empty_dir();
+
+    if (empty_root_entry == -1)
+    {
+        printf(ROOT_TABLE_FULL);
+        return -1;
+    }
+
+    // write file in root directory
+    set_root_table_entry(empty_root_entry, 0, empty_inode, name);
+
+    // inode_table[empty_inode].size = 0;
+
+    int empty_fd_table_entry = find_empty_fd();
+
+    if (empty_fd_table_entry == -1)
+    {
+        printf(TABLE_FULL);
+        return -1;
+    }
+
+    set_fd(empty_fd_table_entry, 0, empty_inode, &inode_table[empty_inode], inode_table[empty_inode].size);
+    write_blocks(1, sb->inode_table_len, inode_table);
+    write_blocks(1 + sb->inode_table_len, DIR_BLOCKS, root);
+
+    return empty_fd_table_entry;
 }
+
 int sfs_fclose(int fileID)
 {
+    D printf("I'm in sfs_fclose\n");
+
+    if (fileID > INODES || fileID < 0)
+    {
+        printf(INVALID_FILE_ID);
+        return -1;
+    }
+
+    set_fd(fileID, 1, -1, NULL, -1);
+    return 0;
 }
+
 int sfs_fread(int fileID, char *buf, int length)
 {
+    D printf("I'm in sfs_fread");
+
+    
 }
+
 int sfs_fwrite(int fileID, const char *buf, int length)
 {
+    D printf("I'm in sfs_fwrite\n");
 }
+
 int sfs_fseek(int fileID, int loc)
 {
+    D printf("I'm in sfs_fseek\n");
 }
+
 int sfs_remove(char *file)
 {
+    D printf("I'm in sfs_remove\n");
 }
 
 #pragma endregion
 
-#pragma HELPER FUNCTIONS
+#pragma region HELPER FUNCTIONS
 
-void superblock_init()
+int store_in_disk()
+{
+    if (write_blocks(0, 1, &sb) == 0 || write_blocks(NUM_BLOCKS - 1, 1, free_bit_map) == 0 || write_blocks(1, FS_SIZE_INITIAL, inode_table) == 0 || write_blocks(FS_SIZE_INITIAL + 1, DIR_BLOCKS, root) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int read_all_from_disk()
+{
+    if (read_blocks(0, 1, &sb) == 0 || read_blocks(NUM_BLOCKS - 1, 1, free_bit_map) == 0 || read_blocks(1, FS_SIZE_INITIAL, inode_table) == 0 || read_blocks(FS_SIZE_INITIAL + 1, DIR_BLOCKS, fd_table) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int find_file_inode_table(char *name)
+{
+    for (int i = 0; i < ARRAYSIZE(root); i++)
+    {
+        if (root[i].empty == 0 && strcmp(root[i].name, name) == 0)
+        {
+            return root[i].num;
+        }
+    }
+
+    return -1;
+}
+
+int find_file_fd_table(int inode_num)
+{
+    for (int i = 0; i < ARRAYSIZE(fd_table); i++)
+    {
+        if (fd_table[i].inodeIndex == inode_num)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int superblock_init()
 {
     sb->magic = MAGIC;
     sb->block_size = BLOCK_SIZE;
-    sb->fs_size = NUM_BLOCKS * BLOCK_SIZE;
+    sb->fs_size = FS_SIZE_INITIAL;
     sb->inode_table_len = INODE_BLOCKS_NO;
     sb->root_dir_inode = 0;
+
+    // TODO: think of some ways this method would fail?
+    return 1;
+}
+
+int set_fd(int fd_index, int empty, uint64_t inodeIndex, inode_t *inode, uint64_t rwptr)
+{
+    fd_table[fd_index] = (file_descriptor){.empty = empty, .inodeIndex = inodeIndex, .inode = inode, .rwptr = rwptr};
+
+    return 1;
+}
+
+int set_root_table_entry(int root_index, int empty, int inode_num, char name[MAX_FILE_NAME])
+{
+    root[root_index] = (directory_entry){.num = inode_num, .empty = empty};
+    strcpy(root[root_index].name, name);
+    // TODO: Maybe null terminate the string?
+
+    return 1;
+}
+
+int find_empty_fd()
+{
+    for (int i = 0; i < ARRAYSIZE(fd_table); i++)
+    {
+        if (fd_table[i].empty == 1)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int find_empty_inode()
+{
+    for (int i = 0; i < ARRAYSIZE(inode_table); i++)
+    {
+        if (inode_table[i].empty == 1)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int find_empty_dir()
+{
+    for (int i = 0; i < ARRAYSIZE(root); i++)
+    {
+        if (root[i].empty == 1)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+#pragma endregion
+
+int main()
+{
+    // mksfs(1);
+    sfs_fopen("text.txt");
+}
+
+#pragma region BITMAP REGION
+
+void force_set_index(uint32_t index)
+{
+    // get index in array of which bit to free
+    uint32_t i = index / 8;
+
+    // get which bit to use
+    uint8_t bit = index % 8;
+
+    // use bit
+    USE_BIT(free_bit_map[i], bit);
+}
+
+uint32_t get_index()
+{
+    uint32_t i = 0;
+
+    // find the first section with a free bit
+    // let's ignore overflow for now...
+    while (free_bit_map[i] == 0)
+    {
+        i++;
+    }
+
+    // now, find the first free bit
+    /*
+        The ffs() function returns the position of the first (least
+       significant) bit set in the word i.  The least significant bit is
+       position 1 and the most significant position is, for example, 32 or
+       64.  
+    */
+    // Since ffs has the lsb as 1, not 0. So we need to subtract
+    uint8_t bit = ffs(free_bit_map[i]) - 1;
+
+    // set the bit to used
+    USE_BIT(free_bit_map[i], bit);
+
+    //return which block we used
+    return i * 8 + bit;
+}
+
+void rm_index(uint32_t index)
+{
+
+    // get index in array of which bit to free
+    uint32_t i = index / 8;
+
+    // get which bit to free
+    uint8_t bit = index % 8;
+
+    // free bit
+    FREE_BIT(free_bit_map[i], bit);
 }
 
 #pragma endregion
--- assignment_3/sfs_api.h
+++ assignment_3/sfs_api.h
@@ -6,9 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fuse.h>
+// #include <fuse.h>
 #include <strings.h>
 #include "disk_emu.h"
+#include <assert.h>
 
 /* macros */
 #define FREE_BIT(_data, _which_bit) \
@@ -21,6 +22,7 @@
 #define IS_ARRAY(arg) (IS_INDEXABLE(arg) && (((void *)&arg) == ((void *)arg)))
 #define ARRAYSIZE(arr) (IS_ARRAY(arr) ? (sizeof(arr) / sizeof(arr[0])) : 0)
 
+#define MAX_FILE_NAME 20
 #define POINTER_SIZE 12
 
 // for debugging purposes
@@ -48,7 +50,7 @@ typedef struct inode_t
     unsigned int size;
     unsigned int data_ptrs[POINTER_SIZE];
     unsigned int indirectPointer; // points to a data block that points to other data blocks (Single indirect)
-    int free;
+    int empty;
 } inode_t;
 
 /*
@@ -58,6 +60,7 @@ typedef struct inode_t
  */
 typedef struct file_descriptor
 {
+    int empty;
     uint64_t inodeIndex;
     inode_t *inode; //
     uint64_t rwptr;
@@ -67,10 +70,9 @@ typedef struct directory_entry
 {
     int num;                  // represents the inode number of the entery.
     char name[MAX_FILE_NAME]; // represents the name of the entery.
-    int free;
+    int empty;
 } directory_entry;
 
-#define MAX_FILE_NAME 20
 #define MAX_EXTENSION_NAME 3
 
 #define LASTNAME_FIRSTNAME_DISK "sfs_disk.disk"
@@ -79,10 +81,12 @@ typedef struct directory_entry
 #define MAGIC 0xACBD0005
 
 // inode constants
-#define INODE_NO 14
-#define INODE_BLOCKS_NO (sizeof(inode_t) * INODE_NO / BLOCK_SIZE + 1)
-#define DIR_BLOCKS_NO (sizeof(entry_t) * INODE_NO / BLOCK_SIZE + 1)
+#define INODES 100
+#define INODE_BLOCK_SIZE 10
+#define INODE_BLOCKS_NO INODES / INODE_BLOCK_SIZE
+#define DIR_BLOCKS (sizeof(directory_entry) * INODES) / BLOCK_SIZE + 1
 #define DUMMY_INITIALIZER -1
+#define FS_SIZE_INITIAL NUM_BLOCKS *BLOCK_SIZE
 
 //maximum number of data blocks on the disk.
 #define BITMAP_ROW_SIZE (NUM_BLOCKS / 8) // this essentially mimcs the number of rows we have in the bitmap. we will have 128 rows.
@@ -96,6 +100,23 @@ int sfs_fread(int fileID, char *buf, int length);
 int sfs_fwrite(int fileID, const char *buf, int length);
 int sfs_fseek(int fileID, int loc);
 int sfs_remove(char *file);
-void init_sb();
+int superblock_init();
+int store_in_disk();
+int set_fd(int fd_index, int empty, uint64_t inodeIndex, inode_t *inode, uint64_t rwptr);
+int find_file_inode_table(char *name);
+int find_file_fd_table(int inode_num);
+int set_root_table_entry(int root_index, int empty, int inode_num, char *name);
+int find_empty_fd();
+int find_empty_inode();
+int find_empty_dir();
+
+// error messages
+#define ERROR "ERROR: "
+#define CANNOT_FIND_FILE ERROR "File could not be found.\n"
+#define INVALID_FILE_NAME ERROR "You have entered an invalid file name. Make sure that it is under 20 characters. \n"
+#define TABLE_FULL ERROR "File descriptor table is full. \n"
+#define INODE_TABLE_FULL ERROR "Inode table is full. \n"
+#define ROOT_TABLE_FULL ERROR "Root table is full. \n"
+#define INVALID_FILE_ID ERROR "You have entered an invalid file ID. \n"
 
 #endif //_INCLUDE_SFS_API_H_
